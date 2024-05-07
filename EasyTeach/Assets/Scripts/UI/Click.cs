c07a3bebb9c8b544f1245777515194dfaa7f5790
loliconriel <loliconriel@gmail.com>
2024-05-07 17:38:11 +0800

    Lesson2 Final

---
+++ EasyTeach/Assets/Scripts/UI/Click.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public void action()
+    {
+        Debug.Log(this.gameObject.name + "Click");
+    }
+}
